b35ad9c1fe2bc18956c567133506d0aea0afd479
unknown <patxiarana05@gmail.com>
2024-08-01 14:37:51 -0300

    class validator

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,8 +1,12 @@
 import { taskStatus } from "../task.entity";
-
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateTaskDTO {
+    @IsString()
+    @IsNotEmpty()
     title:string;
+    @IsString()
+    @IsNotEmpty()
     description:string; 
 
 }
